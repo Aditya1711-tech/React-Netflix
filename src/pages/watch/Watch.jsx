7ce7f97e79b858787a521fb8f32779f1c73fad00
Aditya1711-tech <adityakaneriya1711@gmail.com>
2023-04-06 10:06:07 +0530

    Watch page added

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import "./watch.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import "./watch.scss";
 
-function Watch() {
+export default function Watch() {
   return (
     <div className="watch">
       <div className="back">
         <FontAwesomeIcon icon={faArrowLeft} />
         Home
-        <video
-          className="video"
-          autoPlay
-          progress
-          controls
-          src="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
-        />
       </div>
+      <video
+        className="video"
+        autoPlay
+        progress
+        controls
+        src="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
+      />
     </div>
   );
 }
-
-export default Watch;
