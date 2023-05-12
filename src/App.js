7871b520540a971baf077afeffabe5d264746522
Aditya1711-tech <adityakaneriya1711@gmail.com>
2023-05-12 09:18:21 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <div>
       {/* <Home /> */}
       {/* <Watch /> */}
-      {/* <Register /> */}
-      <Login />
+      <Register />
+      {/* <Login /> */}
     </div>
   );
 }
